a9ac4ed6e70087b4ff01ec12a18265c560915045
ruffaaw <rafalciupek07@gmail.com>
2025-03-16 01:12:09 +0100

    feat: updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Header from "@/components/Header";
 import HomeSection from "@/components/HomeSection";
-// import InvestmentSection from "@/components/InvestmentSection";
+import InvestmentSection from "@/components/InvestmentSection";
 
 export default function Home() {
   return (
     <div className="h-full w-full overflow-hidden">
       <Header />
       <HomeSection />
-      {/* <InvestmentSection /> */}
+      <InvestmentSection />
     </div>
   );
 }
