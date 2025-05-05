18455c52256b76d3e69494e19d4162385b10a428
ruffaaw <rafalciupek07@gmail.com>
2025-05-05 16:24:48 +0200

    fix: fixed problem

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import AboutInvestorSection from "@/components/AboutInvestorSection";
-import Arrangement from "@/components/Arrangement";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-import Gallery from "@/components/Gallery";
-import Header from "@/components/Header";
-import HomeSection from "@/components/HomeSection";
-import House360View from "@/components/House360View";
-import HousesSection from "@/components/HousesSection";
-import InvestmentSection from "@/components/InvestmentSection";
-import TabsSection from "@/components/TabSection";
+// import AboutInvestorSection from "@/components/AboutInvestorSection";
+// import Arrangement from "@/components/Arrangement";
+// import Contact from "@/components/Contact";
+// import Footer from "@/components/Footer";
+// import Gallery from "@/components/Gallery";
+// import Header from "@/components/Header";
+// import HomeSection from "@/components/HomeSection";
+// import House360View from "@/components/House360View";
+// import HousesSection from "@/components/HousesSection";
+// import InvestmentSection from "@/components/InvestmentSection";
+// import TabsSection from "@/components/TabSection";
 import WebsiteUnderConstruction from "@/components/WebsiteUnderConstruction";
 
 export default function Home() {
