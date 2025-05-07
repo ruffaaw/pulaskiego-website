5c064ca7bd6206faa47f94eae775a1d7f2d2385f
ruffaaw <rafalciupek07@gmail.com>
2025-05-07 12:12:00 +0200

    fix: updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-// import AboutInvestorSection from "@/components/AboutInvestorSection";
-// import Arrangement from "@/components/Arrangement";
-// import Contact from "@/components/Contact";
-// import Footer from "@/components/Footer";
-// import Gallery from "@/components/Gallery";
-// import Header from "@/components/Header";
-// import HomeSection from "@/components/HomeSection";
-// import House360View from "@/components/House360View";
-// import HousesSection from "@/components/HousesSection";
-// import InvestmentSection from "@/components/InvestmentSection";
-// import TabsSection from "@/components/TabSection";
-import WebsiteUnderConstruction from "@/components/WebsiteUnderConstruction";
+import AboutInvestorSection from "@/components/AboutInvestorSection";
+import Arrangement from "@/components/Arrangement";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import Gallery from "@/components/Gallery";
+import Header from "@/components/Header";
+import HomeSection from "@/components/HomeSection";
+import House360View from "@/components/House360View";
+import HousesSection from "@/components/HousesSection";
+import InvestmentSection from "@/components/InvestmentSection";
+import TabsSection from "@/components/TabSection";
+// import WebsiteUnderConstruction from "@/components/WebsiteUnderConstruction";
 
 export default function Home() {
   return (
     <div className="h-full w-full overflow-hidden">
-      <WebsiteUnderConstruction />
-      {/* <Header />
+      {/* <WebsiteUnderConstruction /> */}
+      <Header />
       <HomeSection />
       <InvestmentSection />
       <TabsSection />
@@ -25,7 +25,7 @@ export default function Home() {
       <Arrangement />
       <Gallery />
       <Contact />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
