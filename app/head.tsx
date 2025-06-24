4e7f3df0a5416ee2a1025f502612369a241a6085
ruffaaw <rafalciupek07@gmail.com>
2025-06-24 23:16:29 +0200

    feat: updated icon

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@ export const metadata = {
   title: "Pułaskiego 32",
   description:
     "Osiedle Pułaskiego 32 w Wieliczce to nowoczesne, zamknięte osiedle z 28 mieszkaniami. Oferuje prywatność, zieloną przestrzeń, miejsca postojowe i bliskość parków. Sprawdź komfortowe życie w harmonii z naturą!",
-  image: "/logo_biale_ikona.png",
+  image: "/favicon-pulaskiego.ico",
 };
 
 export default function Head() {
